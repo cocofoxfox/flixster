d216bdf6342976bc62fd773b8ce242f64517ef3b
Ning <yanshu.weining@gmail.com>
2019-02-27 19:15:43 -0500

    added Superhero collection

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by ning on 2/27/19.
+//  Copyright © 2019 ning. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
